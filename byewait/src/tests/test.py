2e42767a339c08ff87cd22f3f26dba0c9dcfe55b
juanborssotto <jborssotto@gmail.com>
2018-09-13 14:55:32 -0300

    Agrego que se borren los datos de la tabla managers en el setUp

--- byewait/src/tests/test.py
+++ byewait/src/tests/test.py
@@ -3,7 +3,7 @@ import pymysql
 
 class Test(unittest.TestCase):
     def setUp(self):
-        cnx = pymysql.connect(db="byewait", user="dev", passwd="changeme", port=3306, host="mysql")
+        cnx = pymysql.connect(db="byewait", user="dev", passwd="changeme", port=3306, host="localhost")
         cursor = cnx.cursor()
         cursor.execute('delete from tags_restaurants')
         cursor.execute('delete from scores_item_menu')
@@ -13,6 +13,7 @@ class Test(unittest.TestCase):
         cursor.execute('delete from usuarios')
         cursor.execute('delete from item_menu')
         cursor.execute('delete from categorias')
+        cursor.execute('delete from managers')
         cursor.execute('delete from restaurants')
         # Resetear los auto_increment para que los tests puedan acceder correctamente a los ids
         cursor.execute('ALTER TABLE scores_item_menu AUTO_INCREMENT = 1')
@@ -21,6 +22,7 @@ class Test(unittest.TestCase):
         cursor.execute('ALTER TABLE usuarios AUTO_INCREMENT = 1')
         cursor.execute('ALTER TABLE item_menu AUTO_INCREMENT = 1')
         cursor.execute('ALTER TABLE categorias AUTO_INCREMENT = 1')
+        cursor.execute('ALTER TABLE managers AUTO_INCREMENT = 1')
         cursor.execute('ALTER TABLE restaurants AUTO_INCREMENT = 1')
         cnx.commit()
         cursor.close()
